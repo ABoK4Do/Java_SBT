6111131c6cb41e00c96eaee91855784e64a3912d
ABoK4Do <alesha148@gmail.com>
2018-12-22 20:01:07 +0300

    Добавил private для полей

--- src/test/java/ru/study/homeWork/linkedlist/CustomLinkedListTest.java
+++ src/test/java/ru/study/homeWork/linkedlist/CustomLinkedListTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class CustomLinkedListTest {
-    CustomLinkedList<String> customLinkedList;
+    private CustomLinkedList<String> customLinkedList;
 
     @Before
     public void init() {
