8197da9859ccd789611067ec9c69d583f2c7311c
ABoK4Do <alesha148@gmail.com>
2018-12-24 11:29:03 +0300

    Задание на multithreading Task

---
+++ src/main/java/ru/study/homeWork/multithreading/task/CustomException.java
@@ -0,0 +1,7 @@
+package ru.study.homeWork.multithreading.task;
+
+public class CustomException extends RuntimeException {
+    CustomException(String exceptionMessage) {
+        super(exceptionMessage);
+    }
+}
